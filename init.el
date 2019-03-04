a838b6081c4d919eca109476a355f6da60a54598
yanhelbk <yanhelbk@gmail.com>
2019-03-04 18:23:22 +0800

    update

--- init.el
+++ init.el
@@ -1,62 +1,62 @@
-(setq frame-title-format "emacs")
-
-(menu-bar-mode -1)
-
-(tool-bar-mode -1)
-
-(scroll-bar-mode -1)
-
-(set-default 'cursor-type 'hbar)
-
-(ido-mode)
-
-(column-number-mode)
-
-(show-paren-mode)
-
-(column-number-mode)
-
-(winner-mode t)
-
-(windmove-default-keybindings)
-
-(setq x-select-enable-clipboard t)
-
-(setq x-select-enable-primary t)
-
+;;package init setting
 (require 'package)
 
 (add-to-list 'package-archives
-	     '("melpa" . "http://stable.melpa.org/packages/") t)
+	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
 
-(nlinum-mode)
 
+;;appearance
+(setq frame-title-format "emacs")
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(set-default 'cursor-type 'hbar)
+(column-number-mode)
+(show-paren-mode)
+(column-number-mode)
 (load-theme 'monokai t)
 
+;;window 
+(windmove-default-keybindings)
+(winner-mode t)
+
+;;cutting and pasting 
+(setq x-select-enable-clipboard t)
+(setq x-select-enable-primary t)
+
+;;ido mode
+(ido-mode)
+
+;;smex
 (global-set-key (kbd "M-x") 'smex)
 
+;;ace jump
 (global-set-key (kbd "C-c j") 'ace-jump-mode)
 
+;;customize mark cmd
 (global-set-key (kbd "C-c m") 'set-mark-command)
 
+;;tramp
 (setq tramp-default-method "ssh")
 
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
+;;company
+(add-hook 'c++-mode-hook 'global-company-mode)
+(add-hook 'c-mode-hook 'global-company-mode)
 
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(require 'company-lsp)
+(push 'company-lsp company-backends)
 
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
+;;lsp
+(require 'lsp-mode)
+(add-hook 'c++-mode-hook 'lsp)
+(add-hook 'c-mode-hook 'lsp)
 
-(add-hook 'c++-mode-hook 'company-mode)
-(add-hook 'c-mode-hook 'company-mode)
-(add-hook 'objc-mode-hook 'company-mode)
-
-
+	  
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -67,10 +67,11 @@
     ("5f27195e3f4b85ac50c1e2fac080f0dd6535440891c54fcfa62cdcefedf56b1b" default)))
  '(package-selected-packages
    (quote
-    (rtags magit company-irony tramp-theme smex nlinum monokai-theme irony ace-jump-mode))))
+    (company-lsp lsp-clangd lsp-mode dash cquery rtags magit company-irony tramp-theme smex nlinum monokai-theme irony ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
